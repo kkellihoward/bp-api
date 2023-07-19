ba4ff40fd5afe860b2bd1912ee8441a7340544dd
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-19 03:26:48 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ dotenv.config();
 
 
 // connect to database
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 	.then(() => {
 		// listen for requests
 		app.listen(process.env.PORT, () => {
