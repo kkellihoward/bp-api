67666697873223d53997a5eb1269a68a207dfe37
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-19 03:03:09 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-import accountRoutes from './API/routes/accounts';
+import { accountRoutes } from './API/routes/accounts.js';
 import cors from 'cors';
 import eventRoutes from './API/events';
 
