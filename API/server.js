bbf27ed4a6422fd571efd9b0c51a98f9e2bf65d4
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-19 00:53:51 -0400

    Add files via upload

--- API/server.js
+++ API/server.js
@@ -12,10 +12,20 @@ app.use(cors());
 // middleware
 app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log(req.path, req.method);
-	next();
+app.use((req, res, next) => 
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
 });
+
 
 // routes
 app.use('/accounts', accountRoutes);
