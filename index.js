d5d5f15ebc0c34546c9d033c245c2862dba2b369
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-20 00:04:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,13 @@ app.use(cors());
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
     console.log('Received request:', req.method, req.url, req.params)
     res.end('it works!');
+});
+
+app.use((req, res, next)) => {
+    console.log('Received request:', req.method, req.url, req.params)
 });
 
 // app.use((req, res, next) => 
