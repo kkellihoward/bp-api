c95643a2eaaca4ea8e32d54312325ff23e9b7e80
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-20 00:12:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(cors());
 // middleware
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use((req, res, next) => {
-    console.log('Received request:', req.method, req.url, req.params)
-});
+// app.use((req, res, next) => {
+//     console.log('Received request:', req.method, req.url, req.params)
+// });
 
 app.get('/', (req, res) => {
     console.log('Received request:', req.method, req.url, req.params)
