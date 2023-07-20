580d865bc425cd24f184e919ea00ee28cffbacd3
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-19 22:30:21 -0400

    Update accountController.js

--- API/accountController.js
+++ API/accountController.js
@@ -12,13 +12,14 @@ export const getAccounts = async (req, res) => {
 export const login = async (req, res) => {
     const { username, password } = req.body;
     console.log("Received username:", username); // Add this log to check the received data
-      console.log("Received password:", password);
+    console.log("Received password:", password);
+    
     const user = await Account.findOne({ username });
 
     if (user) {
         if (password === user.password) {
             console.log(user.password + "and" + password);
-            return res.status(200).json({ message: 'You have been successfully lo in!' });
+            return res.status(200).json({ message: 'You have been successfully logged in! ' + String(user.username) + ' ' + String(user.password) });
         } else if (password !== user.password) {
             return res.status(401).json({ error: 'Incorrect password' });
         }
