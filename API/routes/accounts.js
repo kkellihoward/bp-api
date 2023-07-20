a377c367e91162b22073eafcfef93a5443928ddd
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-19 23:40:25 -0400

    Update accounts.js

--- API/routes/accounts.js
+++ API/routes/accounts.js
@@ -17,27 +17,7 @@ const accountRoutes = express.Router();
 accountRoutes.get('/getaccounts', getAccounts);
 
 // GET a single account
-accountRoutes.post('/login', jsonParser, (req, res) => {
-    console.log(req.body);
-    
-    const { username, password } = req.body;
-    console.log("Received username:", username); // Add this log to check the received data
-    console.log("Received password:", password);
-    
-    // const user = await Account.findOne({ username });
-
-    // if (user) {
-    //     if (password === user.password) {
-    //         console.log(user.password + "and" + password);
-    //         return res.status(200).json({ message: 'You have been successfully logged in! ' + String(user.username) + ' ' + String(user.password) + ' ' + String(username) + ' ' + String(password) + ' ' + req.body });
-    //     } else if (password !== user.password) {
-    //         return res.status(401).json({ error: 'Incorrect password' });
-    //     }
-    // } else {
-    //     // return res.status(400).json({ error: String(JSON.parse(req.body)) });
-    //     return res.status(400).json({ error: 'Account does not exist' + ' ' + String(username) + ' ' + String(password) });
-    // }
-});
+accountRoutes.post('/login', login);
 
 // POST a new account
 accountRoutes.post('/signup', createAccount);
