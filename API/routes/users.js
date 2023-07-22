54fc80532bde249cf33599959d4478a725d0d73e
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-21 20:26:18 -0400

    adding fixed files

--- API/routes/users.js
+++ API/routes/users.js
@@ -2,17 +2,12 @@ import express from 'express';
 import {
     signup,
     signin,
-    signout,
-    verifyEmail,
-    resendVerificationEmail,
-    tryReset,
-    resetPassword,
 } from '../controllers/users.js';
 
 const router = express.Router();
 
-router.post('/signin', login);
-router.post('/signup', createAccount);
+router.post('/signin', signin);
+router.post('/signup', signup);
 // router.post('/signout', signout);
 // router.post('/verify-email', verifyEmail);
 // router.post('/resend-verification-email', resendVerificationEmail);
