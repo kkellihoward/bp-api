ccbc721f1e420175f30e5ed688c658418850409b
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-07-27 14:58:32 -0400

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,12 +3,9 @@ import express from 'express';
 import {
 	createAccount,
 	signin,
-	signout
 } from '../controllers/users.js';
 
 const router = express.Router();
-
-router.post('/validate-access', authenticate, finish);
 
 router.post('/createAccount', createAccount);
 router.post('/signin', signin);
