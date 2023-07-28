97d379bfccc0503d6536cfc5d3b50d4de73e25da
lmcarroll987 <137725867+lmcarroll987@users.noreply.github.com>
2023-07-27 20:00:36 -0400

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,11 +3,16 @@ import express from 'express';
 import {
 	createAccount,
 	signin,
+	getAccount,
+	updateAccount
 } from '../controllers/users.js';
 
 const router = express.Router();
 
 router.post('/createAccount', createAccount);
 router.post('/signin', signin);
+
+router.get('/getAccount', getAccount);
+router.get('/updateAccount', updateAccount);
 
 export default router;
