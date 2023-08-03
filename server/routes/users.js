464329ada66d02e1ee7fc4a1b3cdcef98c6ca2aa
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-08-03 06:03:26 -0400

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,15 +4,18 @@ import {
 	createAccount,
 	signin,
 	getAccounts,
-	updateAccount
+	updateAccount,
+	verifyEmail,
+	resendVerificationEmail,
 } from '../controllers/users.js';
 
 const router = express.Router();
 
 router.post('/createAccount', createAccount);
 router.post('/signin', signin);
-
 router.get('/getAccounts', getAccounts);
 router.patch('/updateAccount/:id', updateAccount);
+router.post('/verify-email', verifyEmail);
+router.post('/resend-verification-email', resendVerificationEmail);
 
 export default router;
