592383458dffc81f27f1d22a7365985e6d92cce2
kkellihoward <89551357+kkellihoward@users.noreply.github.com>
2023-08-03 06:28:12 -0400

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -15,7 +15,7 @@ router.post('/createAccount', createAccount);
 router.post('/signin', signin);
 router.get('/getAccounts', getAccounts);
 router.patch('/updateAccount/:id', updateAccount);
-router.get('/verify-email', verifyEmail);
-router.get('/resend-verification-email', resendVerificationEmail);
+router.post('/verify-email', verifyEmail);
+router.post('/resend-verification-email', resendVerificationEmail);
 
 export default router;
